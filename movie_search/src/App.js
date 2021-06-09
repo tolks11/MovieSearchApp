42fda093566fef4273ab1d74a99157943f8185ef
tolks <62411436+tolks11@users.noreply.github.com>
2021-06-09 13:42:41 -0400

    Add files via upload
    MVP for user display and proper functionality

---
+++ movie_search/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react'
+import SearchMovies from './SearchMovies'
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">movie app</h1>
+      <SearchMovies />
+    </div>
+  );
+}
+
+export default App;
